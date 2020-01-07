c7b46d76177a4983152acc60fcfce6599b09980a
Muhammad <muhammad@ai.astra.co.id>
2020-01-07 18:03:31 +0700

    on route course

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* Switch : only shows 1 component that first matches the path */}
+      <Switch>
+        {/* exact : shows the component when have exact path, if disable, path="/hats" showing the HomePage as well */}
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
