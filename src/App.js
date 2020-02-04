b8c0380214006faee53901a2682c15b7d0faa6bc
muhfs94 <muhfs94@gmail.com>
2020-02-04 13:47:07 +0700

    creating signin-and-signup components, form-input, custom-button and signin components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from "./components/header/header.component";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         {/* exact : shows the component when have exact path, if disable, path="/hats" showing the HomePage as well */}
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
