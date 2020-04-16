f7676a06268e9d4a73c0fefd0a8f407339877b66
muhfs94 <muhfs94@gmail.com>
2020-04-16 16:43:13 +0700

    Updated the code to leverage containers for collections-overview and collection-page components

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -2,17 +2,17 @@ import { compose } from "redux";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
+import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionPage from "./collection.component";
 
 const mapStateToProps = createStructuredSelector({
-  isLoading: selectIsCollectionFetching
+  isLoading: state => !selectIsCollectionsLoaded(state)
 });
 
-const CollectionsOverviewContainer = compose(
+const CollectionPageContainer = compose(
   connect(mapStateToProps),
   WithSpinner
 )(CollectionPage);
 
-export default CollectionsOverviewContainer;
+export default CollectionPageContainer;
